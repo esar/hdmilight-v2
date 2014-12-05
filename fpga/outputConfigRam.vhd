a95e7fd829bcc4d04aa6afee7958c4c8004a2db5
esar <hacks@esar.org.uk>
2014-12-05 23:00:17 +0000

    simplified outputConfigRam implementation

--- fpga/outputConfigRam.vhd
+++ fpga/outputConfigRam.vhd
@@ -41,20 +41,12 @@ entity outputConfigRam is
 end outputConfigRam;
 
 architecture Behavioral of outputConfigRam is
-	type mem_t is array (0 to 2047) of std_logic_vector(7 downto 0);
-	shared variable memOdd1  : mem_t;
-	shared variable memOdd2  : mem_t;
-	shared variable memEven1 : mem_t;
-	shared variable memEven2 : mem_t;
+	type mem_t is array (0 to 4095) of std_logic_vector(7 downto 0);
+	shared variable memOdd  : mem_t;
+	shared variable memEven : mem_t;
 
-	signal a_doutOdd1 : std_logic_vector(7 downto 0);
-	signal a_doutOdd2 : std_logic_vector(7 downto 0);
-	signal a_doutEven1 : std_logic_vector(7 downto 0);
-	signal a_doutEven2 : std_logic_vector(7 downto 0);
-	signal a_sel : std_logic_vector(1 downto 0);
-
-	signal b_data1 : std_logic_vector(15 downto 0);
-	signal b_data2 : std_logic_vector(15 downto 0);
+	signal a_doutOdd  : std_logic_vector(7 downto 0);
+	signal a_doutEven : std_logic_vector(7 downto 0);
 begin
 
 
@@ -62,86 +54,44 @@ begin
 process(a_clk)
 begin
 	if(rising_edge(a_clk)) then
-		if(a_addr(0) = '0' and a_addr(12) = '0') then
+		if(a_addr(0) = '0') then
 			if(a_wr = '1') then
-				memOdd1(conv_integer(a_addr(11 downto 1))) := a_din;
+				memOdd(conv_integer(a_addr(12 downto 1))) := a_din;
 			end if;
 		end if;
-		a_doutOdd1 <= memOdd1(conv_integer(a_addr(11 downto 1)));
-	end if;
-end process;
-process(a_clk)
-begin
-	if(rising_edge(a_clk)) then
-		if(a_addr(0) = '0' and a_addr(12) = '1') then
-			if(a_wr = '1') then
-				memOdd2(conv_integer(a_addr(11 downto 1))) := a_din;
-			end if;
-		end if;
-		a_doutOdd2 <= memOdd2(conv_integer(a_addr(11 downto 1)));
+		a_doutOdd <= memOdd(conv_integer(a_addr(12 downto 1)));
 	end if;
 end process;
 
 process(a_clk)
 begin
 	if(rising_edge(a_clk)) then
-		if(a_addr(0) = '1' and a_addr(12) = '0') then
+		if(a_addr(0) = '1') then
 			if(a_wr = '1') then
-				memEven1(conv_integer(a_addr(11 downto 1))) := a_din;
+				memEven(conv_integer(a_addr(12 downto 1))) := a_din;
 			end if;
 		end if;
-		a_doutEven1 <= memEven1(conv_integer(a_addr(11 downto 1)));
+		a_doutEven <= memEven(conv_integer(a_addr(12 downto 1)));
 	end if;
 end process;
 
-process(a_clk)
-begin
-	if(rising_edge(a_clk)) then
-		if(a_addr(0) = '1' and a_addr(12) = '1') then
-			if(a_wr = '1') then
-				memEven2(conv_integer(a_addr(11 downto 1))) := a_din;
-			end if;
-		end if;
-		a_doutEven2 <= memEven2(conv_integer(a_addr(11 downto 1)));
-	end if;
-end process;
-
-a_sel <= a_addr(12) & a_addr(0);
-with a_sel select a_dout <=
-	a_doutOdd1 when "00",
-	a_doutEven1 when "01",
-	a_doutOdd2 when "10",
-	a_doutEven2 when "11";
+a_dout <= a_doutOdd when a_addr(0) = '0' else a_doutEven;
  
+
 -- Port B
 process(b_clk)
 begin
 	if(rising_edge(b_clk)) then
-		b_data1( 7 downto  0) <= memOdd1(conv_integer(b_addr(10 downto 0)));
+		b_data( 7 downto  0) <= memOdd(conv_integer(b_addr(11 downto 0)));
 	end if;
 end process;
 
 process(b_clk)
 begin
 	if(rising_edge(b_clk)) then
-		b_data2( 7 downto  0) <= memOdd2(conv_integer(b_addr(10 downto 0)));
-	end if;
-end process;
-
-process(b_clk)
-begin
-	if(rising_edge(b_clk)) then
-		b_data1(15 downto  8) <= memEven1(conv_integer(b_addr(10 downto 0)));
+		b_data(15 downto  8) <= memEven(conv_integer(b_addr(11 downto 0)));
 	end if;
 end process;
  
-process(b_clk)
-begin
-	if(rising_edge(b_clk)) then
-		b_data2(15 downto  8) <= memEven2(conv_integer(b_addr(10 downto 0)));
-	end if;
-end process;
 
-b_data <= b_data1 when b_addr(11) = '0' else b_data2;
-	
 end Behavioral;
