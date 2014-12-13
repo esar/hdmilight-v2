78e1ec98573d8f6b106c5ce3bd20a0f292bc9ad0
esar <hacks@esar.org.uk>
2014-12-13 12:02:58 +0000

    added brightness/contrast/saturation control to colour config files

--- tools/colour2bin.py
+++ tools/colour2bin.py
@@ -1,22 +1,92 @@
 #!/usr/bin/python
 
 import sys
+import numpy as np
+
+# weights taken from http://graficaobscura.com/matrix/index.html
+WR = 0.3086
+WG = 0.6094
+WB = 0.0820
+
+class Values:
+	def __init__(self):
+		self.matrixR = [1.0, 0.0, 0.0]
+		self.matrixG = [0.0, 1.0, 0.0]
+		self.matrixB = [0.0, 0.0, 1.0]
+		self.matrixC = [0.0, 0.0, 0.0]
+		self.brightR = 1.0
+		self.brightG = 1.0
+		self.brightB = 1.0
+		self.contrast = 1.0
+		self.saturation = 1.0
+		
+values = Values()
 
 input = open(sys.argv[1])
-for line in input:
+for (linenum, line) in enumerate(input):
 	line = line[:line.find('#')]
-	cols = line.split()
-	if len(cols) == 0:
+	parts = line.split(None, 2)
+	if len(parts) == 0:
 		continue
 
-	(r, g, b) = [long(float(x) * 512) for x in cols]
+	if len(parts) != 3 or parts[1] != '=':
+		sys.stderr.write('expected name = value pair at line %s\n' % linenum)
+		sys.stderr.write('    %s\n' % line)
+		sys.exit(1)
+	
+	if not hasattr(values, parts[0]):
+		sys.stderr.write('unknown attribute "%s" at line %s\n' % (parts[0], linenum))
+		sys.exit(1)
 
-	r &= 0x3ffff
-	g &= 0x3ffff
-	b &= 0x3ffff
+	t = type(getattr(values, parts[0]))
+	if t == list:
+		lt = type(getattr(values, parts[0])[0])
+		setattr(values, parts[0], [lt(x) for x in parts[2].split()])
+	else:
+		setattr(values, parts[0], t(parts[2]))
+
+
+values.matrixR.append(0.0)
+values.matrixG.append(0.0)
+values.matrixB.append(0.0)
+values.matrixC.append(1.0)
+mat = np.matrix([values.matrixR,
+                 values.matrixG,
+                 values.matrixB,
+                 values.matrixC])
+
+br = values.brightR
+bg = values.brightG
+bb = values.brightB
+bmat = np.matrix([[br, 0,  0,  0], 
+                  [0,  bg, 0,  0], 
+                  [0,  0,  bb, 0], 
+                  [0,  0,  0,  1]])
+
+c = values.contrast
+t = (1.0 - c) / 2.0
+cmat = np.matrix([[c, 0, 0, 0],
+                  [0, c, 0, 0],
+                  [0, 0, c, 0],
+                  [t, t, t, 1]])
+
+
+s = values.saturation
+si = 1.0 - s
+smat = np.matrix([[si * WR + s, si * WR,     si * WR,     0],
+                  [si * WG,     si * WG + s, si * WG,     0],
+                  [si * WB,     si * WB,     si * WB + s, 0],
+                  [0,           0,           0,           1]])
+
+result = mat * bmat * cmat * smat
+
+#sys.stderr.write(str(result) + '\n')
+
+for row in result.tolist():
+	(r, g, b, c) = [long(float(x) * 512) & 0x3ffff for x in row]
 
 	data = (b << 36) | (g << 18) | r
-	
+
 	sys.stdout.write(chr(data & 0xff))
 	sys.stdout.write(chr((data >>  8) & 0xff))
 	sys.stdout.write(chr((data >> 16) & 0xff))
