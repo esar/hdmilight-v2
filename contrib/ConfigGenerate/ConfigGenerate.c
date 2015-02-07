9ce430df5e8e65eeaaf736d746be8b1108f71f58
jetty840 <clelland283@gmail.com>
2015-02-06 21:32:12 -0700

    Adding Makefile and license to ConfigGenerate.c

--- contrib/ConfigGenerate/ConfigGenerate.c
+++ contrib/ConfigGenerate/ConfigGenerate.c
@@ -1,4 +1,24 @@
-// Copyright Jetty, Feb 4th 2015
+/*
+
+   Copyright (C) 2015 Jetty
+
+   This file is part of HDMI-Light
+
+   HDMI-Light is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   HDMI-Light is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this code (see the file names COPING).
+   If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #include <stdio.h>
 #include <math.h>
